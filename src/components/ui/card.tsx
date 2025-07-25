47b8032c208a0e17afade04accc2aa770b40bb58
denny saputra <detrastudios@gmail.com>
2025-07-25 23:15:42 +0000

    pada mode gelap hilangkan outlin pada card

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-lg dark:shadow-glow",
+      "rounded-lg border bg-card text-card-foreground shadow-lg dark:border-0 dark:shadow-glow",
       className
     )}
     {...props}

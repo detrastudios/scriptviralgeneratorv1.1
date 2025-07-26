f652483eb7f461127f0c59467694cb1e1ee63e1f
denny saputra <detrastudios@gmail.com>
2025-07-26 01:43:16 +0000

    Visig V1.1 linkan ke http://lynk.id/detrastudios/4lmdwj59qkp9

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,14 @@ export function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-center gap-2 md:h-24">
-        <p className="text-xs font-semibold text-muted-foreground">Visig V 1.1</p>
+        <a
+          href="http://lynk.id/detrastudios/4lmdwj59qkp9"
+          target="_blank"
+          rel="noreferrer"
+          className="text-xs font-semibold text-muted-foreground underline-offset-4 hover:underline"
+        >
+          Visig V 1.1
+        </a>
         <p className="text-balance text-center text-xs leading-loose text-muted-foreground">
           © 2025{' '}
           <a
